9bdfbeec1e4312d60b99f286b1e8f52a5e4672fe
Tibesnoff <Tibesnoff@gmail.com>
2024-01-31 17:35:48 -0500

    Update header and weather banner styles

--- WebApp/src/components/weather/WeatherBanner.tsx
+++ WebApp/src/components/weather/WeatherBanner.tsx
@@ -7,32 +7,53 @@ import { Period } from "../../types/weatherType";
 const WeatherBanner = () => {
     const {weatherData} = React.useContext(context) as AppContextType;
 
+    function getHighLow(description: string) {
+        const tempMatches = description.match(/\b\d+\b(?!\s*mph)/g);
+    
+        if (!tempMatches) {
+            return { highTemp: null, lowTemp: null };
+        }
+    
+        const temps = tempMatches.map(Number);
+        const highTemp = Math.max(...temps);
+        const lowTemp = Math.min(...temps);
+    
+        return { highTemp, lowTemp };
+    }
+
     const currentForcast = () => {
         const period = weatherData.periods[0];
         return (
-            <div className="h-full w-1/4 flex flex-col items-center">
-                {period.detailedForecast.split('. ').map((sentence:string, index:number) => (
-                index < 3 && <Text key={index} classNameProps="text-sm line-clamp-2 text-center" content={sentence}/>
-            ))}
+            <div className="h-full w-1/4 flex flex-col items-center rounded-md">
+                {
+                    period.detailedForecast.split('. ').map((sentence:string, index:number) => (
+                        index < 3 && <Text key={index} classNameProps="text-sm line-clamp-2 text-center" content={sentence}/>
+                    ))
+                }
             </div>
         )
     }
 
     const dayForcast = (period: Period, index: number) => {
+        const highLow = getHighLow(period.detailedForecast);
         return (
-            <div className={`h-full w-2/6 ml-2 mr-2 flex flex-col items-center ${index % 2 === 0?"bg-blue-600":"bg-blue-500"}`}>
+            <div key={index} className={`h-full w-2/6 ml-2 mr-2 flex flex-col items-center rounded-md ${index % 2 === 0?"bg-blue-600":"bg-blue-500"}`}>
                 <Text classNameProps="font-bold text-sm" content={period.name}/>
                 <Text classNameProps="text-sm line-clamp-1" content={period.shortForecast}/>
-                <Text classNameProps="text-sm" content={period.temperature + "°"}/>
+                <div className="flex flex-row space-x-3">
+                    <Text classNameProps="text-sm" content={`H: ${highLow.highTemp}°`}/>
+                    <Text classNameProps="text-sm" content={`L: ${highLow.lowTemp}°`}/>
+                </div>
+                
             </div>
         )
     }
 
     const weekForcast = () => {
         return (
-            <div className="h-full w-3/4 pl-4 pr-4 flex flex-row justify-end">
+            <div className="h-full w-3/4 pl-4 pr-4 flex flex-row justify-start">
                 {
-                    weatherData.periods && weatherData.periods.map((period: Period, index: number) => index !== 0 && dayForcast(period, index))
+                    weatherData.periods.map((period: Period, index: number) => index !== 0 && dayForcast(period, index))
                 }
             </div>
         )
@@ -40,8 +61,8 @@ const WeatherBanner = () => {
 
     const forcast = () =>{
         return (
-            <div className="h-full w-full pl-4 pr-4 flex flex-row justify-center bg-blue-700">
-                {weatherData.periods && currentForcast()}
+            <div className="h-fit w-full pl-4 pr-4 flex flex-row justify-center items-center bg-blue-700 rounded-md">
+                {currentForcast()}
                 {weekForcast()}
             </div>
         )
@@ -49,7 +70,7 @@ const WeatherBanner = () => {
 
     const titles = () => {
         return (
-            <div className="h-fit w-full flex-row flex pl-2 pr-2"> 
+            <div className="h-fit w-full flex-row flex pl-2 pr-2 mb-2"> 
                 <div className="h-fit w-1/4 text-center">
                     <Text classNameProps="font-bold text-xl" content="Current Weather in Pleasentville"/>
                 </div>
@@ -61,9 +82,9 @@ const WeatherBanner = () => {
     }
 
     return (
-        <div className="h-28 m-2 flex flex-col bg-blue-900 p-2 pt-0 items-center">
+        <div className="h-30 m-2 flex flex-col bg-blue-900 p-2 pt-0 items-center rounded-md">
             {titles()}
-            {forcast()}
+            {weatherData.periods && forcast()}
         </div>
     )
 }
--- WebApp/src/stylizedComponents/Text.tsx
+++ WebApp/src/stylizedComponents/Text.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 interface TextProps {
   classNameProps: string;
   content: string;
+  colorOverride?: boolean;
 }
 
-const Text: React.FC<TextProps> = ({ classNameProps, content }) => {
-  return <p className={`${classNameProps} text-slate-50`}>{content}</p>;
+const Text: React.FC<TextProps> = ({ classNameProps, content, colorOverride = false }) => {
+  return <p className={`${classNameProps} ${colorOverride?'':'text-slate-50'}`}>{content}</p>;
 };
 
 export default Text;
