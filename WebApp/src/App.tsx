e958553d87901610d20e7dbe4d45454c1d1d460a
Tibesnoff <Tibesnoff@gmail.com>
2024-02-04 16:14:22 -0500

    Add Section component to App

--- WebApp/src/App.tsx
+++ WebApp/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './components/header/Header';
 import GlobalState from './context/GlobalState';
+import Section from './components/middle/section';
 
 function App() {
   return (
     <GlobalState>
-      <div className='h-screen'><Header/></div>
+      <div className='h-screen'><Header/><Section/></div>
     </GlobalState>
   );
 }
