8a70dfa21fe1d9b2e35b2b82afce906f7ad631f0
Tibesnoff <Tibesnoff@gmail.com>
2024-02-04 19:14:25 -0500

    Add Run.bat script for LC_Feed Viewer

---
+++ Scripts/Run.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo Welcome to LC_Feed Viewer!
+
+set /P choice=Select which LC_Feed you would like to see (1 for New, 2 for Old): 
+
+if !choice! equ 1 (
+    echo Running New LC_Feed...
+    cd ../WebApp
+
+    call git checkout main
+    call git pull
+    call yarn install
+    call yarn start
+) else if !choice! equ 2 (
+    echo Running Old LC_Feed...
+    cd ../Old_LC_Feed
+    start "" "index.html"
+) else (
+    echo Invalid choice. Please enter 1 or 2.
+)
+
+endlocal
