87a554d8a92768f7fda9d10fb765e77d3879a2a5
Ed_Media <Ed_Media>
2024-02-26 12:36:53 -0500

    Refactor time formatting in GlobalState.tsx

--- WebApp/src/context/GlobalState.tsx
+++ WebApp/src/context/GlobalState.tsx
@@ -85,8 +85,10 @@ const GlobalState: React.FC<PropsWithChildren> = ({ children }) => {
       const hours = now.getHours().toString().padStart(2, '0');
       const minutes = now.getMinutes().toString().padStart(2, '0');
       const seconds = now.getSeconds().toString().padStart(2, '0');
-      
-      setCurrentTime(`${+hours <= 12 ? hours : (+hours - 12).toString().padStart(2, '0') }:${minutes}:${seconds} ${+hours < 12 ? "AM" : "PM" }`);
+
+      setCurrentTime(
+        `${+hours <= 12 ? hours : (+hours - 12).toString().padStart(2, '0')}:${minutes}:${seconds} ${+hours < 12 ? 'AM' : 'PM'}`
+      );
 
       // so it updates the date every day without having to refresh the page
       const formattedDate = now.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' });
